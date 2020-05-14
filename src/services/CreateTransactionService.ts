e6d5178b0a66b302758b4bde998f77ae4447f835
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2020-05-14 05:10:33 -0300

    Handling transaction creation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Unrecognized transaction type');
+    }
+
+    if (type === 'outcome') {
+      const totalAvaliable = this.transactionsRepository.getBalance().total;
+
+      if (totalAvaliable < value || totalAvaliable - value < 0) {
+        throw Error(
+          'This transaction cannot be created since you dont have enough income to take',
+        );
+      }
+    }
+
+    const newTransaction = new Transaction({ title, value, type });
+    this.transactionsRepository.create(newTransaction);
+
+    return newTransaction;
   }
 }
 
